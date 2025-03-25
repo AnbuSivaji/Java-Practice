a0e6f213cc63ed2adcb67d2c689bca2a1c986cbd
AnbuSivaji <anbusoft57@gmail.com>
2025-03-25 15:30:35 +0530

    adfghvasjdvjas

--- add2.java
+++ add2.java
@@ -1,9 +1,11 @@
 public class add2 {
     public static void main(String args[]){
-        int num=1234;
-        StringBuffer sb=new StringBuffer(String.valueOf(num));
-        StringBuffer rev=sb.reverse();
-        System.out.println("Reverse of the number is:"+rev);
+        
+        int a=10;
+        int b=25;
+        int c;
+        c=a+b;
+        System.out.println("The sum of the numbers is: "+c);
     }
     
 }
