6141cc4878891a175423f763d811d009da55768e
AnbuSivaji <anbusoft57@gmail.com>
2025-03-28 14:48:36 +0530

    Switch model

---
+++ SwitchCase.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SwitchCase {
+     public static void main(String args[]){
+        Scanner Vowel=new Scanner (System.in);
+        System.out.println("Enter a character : ");
+        char ch=Vowel.next().charAt(0);
+        switch(ch){
+              case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' -> System.out.println("The character is a vowel");
+              default -> System.out.println("The vhracter is a constant");
+          
+        }
+
+
+
+    }
+}
